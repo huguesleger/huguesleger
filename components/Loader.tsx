82313df06c0aa9ca15a200304b1741fb37884661
hugues leger <hugueslegerdevweb@gmail.com>
2023-10-03 18:57:57 +0200

    add anim remove loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,32 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useRef } from "react";
 import ChangeWord from "./ChangeWord";
+import gsap from "gsap";
 
 const Loader = () => {
+  const refLoader = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.to(refLoader.current, {
+      delay: 7,
+      opacity: 0,
+      ease: "Power2.easeInOut",
+      duration: 0.5,
+      onComplete: () => {
+        gsap.to(refLoader.current, {
+          display: "none",
+          delay: 0.3,
+        });
+      },
+    });
+  }, []);
+
   return (
-    <div className="loader-wrapper">
+    <div className="loader-wrapper" ref={refLoader}>
       <div className="loader">
         <div className="loader-inner">
           <div className="wrap-words">
