5f626622f30c9b547fac292823078e759f710a79
hugues leger <hugueslegerdevweb@gmail.com>
2023-10-24 20:36:25 +0200

    remove loader is not homepage

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,47 +1,57 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import ChangeWord from "./ChangeWord";
+import { usePathname } from "next/navigation";
 import gsap from "gsap";
 
 const Loader = () => {
   const refLoader = useRef(null);
 
+  const pathname = usePathname();
+
   useEffect(() => {
     const tl = gsap.timeline();
 
-    tl.to(refLoader.current, {
-      delay: 7,
-      opacity: 0,
-      ease: "Power2.easeInOut",
-      duration: 0.5,
-      onComplete: () => {
-        gsap.to(refLoader.current, {
-          display: "none",
-          delay: 0.3,
-        });
-      },
-    });
-  }, []);
+    if (pathname === "/") {
+      tl.to(refLoader.current, {
+        delay: 7,
+        opacity: 0,
+        ease: "Power2.easeInOut",
+        duration: 0.5,
+        onComplete: () => {
+          gsap.to(refLoader.current, {
+            display: "none",
+            delay: 0.3,
+          });
+        },
+      });
+    }
+  }, [refLoader.current]);
 
   return (
-    <div className="loader-wrapper" ref={refLoader}>
-      <div className="loader">
-        <div className="loader-inner">
-          <div className="wrap-words">
-            <ChangeWord
-              words={[
-                "hugues Leger",
-                "Hello",
-                "Salut",
-                "Créatif Développeur front",
-              ]}
-            />
+    <>
+      {pathname === "/" ? (
+        <div className="loader-wrapper" ref={refLoader}>
+          <div className="loader">
+            <div className="loader-inner">
+              <div className="wrap-words">
+                <ChangeWord
+                  words={[
+                    "hugues Leger",
+                    "Hello",
+                    "Salut",
+                    "Créatif Développeur front",
+                  ]}
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
