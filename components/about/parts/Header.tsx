2f96dee6bbdc8be80c94c1430d44b73a964f4fff
hugues leger <hugueslegerdevweb@gmail.com>
2023-10-26 20:28:42 +0200

    add style header page about

--- components/about/parts/Header.tsx
+++ components/about/parts/Header.tsx
@@ -1,11 +1,32 @@
+import SplittingWrapperWord from "@/components/splitting/SplittingWrapperWord";
 import React from "react";
 
 const Header = ({ props }: any) => {
   const about = props;
   return (
-    <div>
-      <p>{about.titre}</p>
-      <p>{about.titre2}</p>
+    <div className="wrapper" data-scroll-section>
+      <div className="container">
+        <div className="wrap-title-about">
+          <h1 className="title-about" data-scroll>
+            <span
+              className="title-1"
+              data-scroll
+              data-scroll-speed="3"
+              data-scroll-position="top"
+            >
+              <SplittingWrapperWord>{about.titre}</SplittingWrapperWord>
+            </span>
+            <span
+              className="title-2"
+              data-scroll
+              data-scroll-speed="2"
+              data-scroll-position="top"
+            >
+              <SplittingWrapperWord>{about.titre2}</SplittingWrapperWord>
+            </span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
