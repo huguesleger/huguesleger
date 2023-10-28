d70c2983680b5ed6e5384b3341a524318a6aa044
hugues leger <hugueslegerdevweb@gmail.com>
2023-10-28 11:11:49 +0200

    add component contact page about

--- components/about/AboutPage.tsx
+++ components/about/AboutPage.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useAppContext } from "@/app/context/AppContext";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Bio from "./parts/Bio";
 import CircleText from "./parts/CircleText";
 import Competences from "./parts/competences/Competences";
+import Contact from "./parts/Contact";
 import Cursus from "./parts/Cursus";
 import Header from "./parts/Header";
 
 const AboutPage = ({ data }: any) => {
   const { setPageName } = useAppContext();
+  const refContact = useRef(null);
 
   useEffect(() => {
     setPageName("page-about");
@@ -22,6 +24,14 @@ const AboutPage = ({ data }: any) => {
       <CircleText props={data} />
       <Cursus props={data} />
       <Competences props={data} />
+      <div
+        className="section-contact"
+        ref={refContact}
+        data-scroll-section
+        data-scroll
+      >
+        <Contact props={data} />
+      </div>
     </div>
   );
 };
---
+++ components/about/parts/Contact.tsx
@@ -0,0 +1,36 @@
+import CopyEmail from "@/components/CopyEmail";
+import Footer from "@/components/layout/Footer";
+import Link from "next/link";
+import React from "react";
+
+const Contact = ({ props }: any) => {
+  const contact = props;
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="inner-contact">
+          <div
+            className="wrap-content"
+            data-scroll
+            data-scroll-speed="-4"
+            data-scroll-position="bottom"
+          >
+            <div className="inner-title" data-scroll>
+              <h2 className="title-contact" data-scroll data-scroll-speed="3">
+                {contact.titreContact}
+              </h2>
+            </div>
+            <div className="wrap-email">
+              <CopyEmail>{contact.emailContact}</CopyEmail>
+            </div>
+          </div>
+          <div className="inner-footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
