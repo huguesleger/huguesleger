7c30bd6b8a2eb68f8eba15e3f61948be0d2d708a
hugues leger <hugueslegerdevweb@gmail.com>
2023-10-29 13:44:40 +0100

    add component infos page realisation

--- components/realisation/RealisationPage.tsx
+++ components/realisation/RealisationPage.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+
+import { useAppContext } from "@/app/context/AppContext";
+import React, { useEffect } from "react";
 import Header from "./parts/Header";
+import Infos from "./parts/Infos";
 
 export default function RealisationPage({ data }: any) {
+  const { setPageName } = useAppContext();
+
+  useEffect(() => {
+    setPageName("page-realisation-detail");
+  }, []);
+
   return (
     <>
       <Header props={data} />
+      <Infos props={data} />
     </>
   );
 }
---
+++ components/realisation/parts/Infos.tsx
@@ -0,0 +1,62 @@
+import SplittingWrapperWord from "@/components/splitting/SplittingWrapperWord";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Infos = ({ props }: any) => {
+  const infos = props;
+  return (
+    <section
+      className={`project-infos ${infos.colorText}`}
+      data-scroll-section
+      style={{ backgroundColor: infos.colorSection.hex }}
+    >
+      <div className="wrapper">
+        <div className="container">
+          <div className="wrap-content">
+            <div className="wrap-title">
+              <div className="inner-title">
+                <h1 className="title-project">
+                  <SplittingWrapperWord>{infos.titre}</SplittingWrapperWord>
+                </h1>
+              </div>
+            </div>
+            <div className="wrap-infos">
+              {infos.siteWeb && (
+                <div className="inner-info">
+                  <p className="name-info">Site web</p>
+                  <Link
+                    href={`https://${infos.siteWeb}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="link-website"
+                  >
+                    <span className="link-text">Voir le site</span>
+                    <span className="link-arrow">
+                      <Image
+                        src="/images/link-arrow-white.svg"
+                        width={24}
+                        height={24}
+                        alt="Voir le site"
+                      />
+                    </span>
+                  </Link>
+                </div>
+              )}
+              <div className="inner-info">
+                <p className="name-info">Intervention</p>
+                <p>{infos.intervention}</p>
+              </div>
+              <div className="inner-info">
+                <p className="name-info">Année</p>
+                <p>{infos.annee}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Infos;
